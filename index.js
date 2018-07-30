6f05f878a170d70ed92bb3297d26676c44e447d7
Andreas Linnert <andreas.linnert@nbsp.de>
2018-07-30 16:47:45 +0200

    Replaced images with simple-icons, removed image files

--- index.js
+++ index.js
@@ -3,11 +3,12 @@ const path = require('path')
 const fs = require('fs')
 const json5 = require('json5')
 const htmlMinifier = require('html-minifier')
+const simpleIcons = require('simple-icons')
 
 edge.registerViews(path.resolve(`${__dirname}/templates`))
 const dataString = fs.readFileSync(path.resolve(`${__dirname}/data/sites.json5`))
 const data = json5.parse(dataString)
-const html = edge.render('index', data)
+const html = edge.render('index', { ...data, simpleIcons })
 const minifiedHtml = htmlMinifier.minify(html, {
   collapseBooleanAttributes: true,
   collapseInlineTagWhitespace: true,
