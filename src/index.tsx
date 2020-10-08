dfcf1a4fa81b1cd099002bc5e25ca82b7913c785
Andreas Linnert <andreas@linnertmedia.de>
2020-10-08 22:32:46 +0200

    Fix typo in code

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { WebdevHome } from './App'
 import './sass/index.scss'
 
-ReactDOM.render(<ebdevHome />, document.getElementById('root'))
+ReactDOM.render(<WebdevHome />, document.getElementById('root'))
