85b0ba93baec53a861767f8bc909a9339c1465fd
Andreas Linnert <andreas@linnertmedia.de>
2026-05-19 17:49:49 +0200

    Update logo gradient

--- src/header/Logo.tsx
+++ src/header/Logo.tsx
@@ -8,8 +8,8 @@ export const Logo: FC = () => {
       className={classNames(
         'text-lg font-semibold tracking-wide text-nowrap select-none',
         'bg-cover bg-clip-text text-transparent',
-        'bg-[linear-gradient(70deg_in_oklch_decreasing_hue,oklch(0.4_0.18_240),oklch(0.4_0.18_40))]',
-        'dark:bg-[linear-gradient(70deg_in_oklch_decreasing_hue,oklch(0.75_0.1_240),oklch(0.75_0.1_40))]',
+        'bg-[linear-gradient(70deg_in_oklch,oklch(0.4_0.14_240),oklch(0.45_0.12_200),oklch(0.5_0.12_100),oklch(0.45_0.1_340))]',
+        'dark:bg-[linear-gradient(70deg_in_oklch,oklch(0.9_0.1_240),oklch(0.95_0.1_200),oklch(0.95_0.1_100),oklch(0.85_0.1_340))]',
       )}
     >
       {appConfig.appTitle}
