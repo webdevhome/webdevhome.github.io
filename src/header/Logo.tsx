3d1ca2c690b230dbabeca3a7d80dcde3a81cf1fb
Andreas Linnert <andreas@linnertmedia.de>
2026-05-29 12:06:13 +0200

    Update logo colors

--- src/header/Logo.tsx
+++ src/header/Logo.tsx
@@ -8,8 +8,8 @@ export const Logo: FC = () => {
       className={classNames(
         'text-lg font-semibold tracking-wide text-nowrap select-none',
         'bg-cover bg-clip-text text-transparent',
-        'bg-[linear-gradient(70deg_in_oklch,oklch(0.4_0.14_240),oklch(0.45_0.12_200),oklch(0.5_0.12_100),oklch(0.45_0.1_340))]',
-        'dark:bg-[linear-gradient(70deg_in_oklch,oklch(0.9_0.1_240),oklch(0.95_0.1_200),oklch(0.95_0.1_100),oklch(0.85_0.1_340))]',
+        'bg-[linear-gradient(70deg_in_oklch,oklch(0.47_0.16_190),oklch(0.45_0.16_260))]',
+        'dark:bg-[linear-gradient(70deg_in_oklch,oklch(0.7_0.17_190),oklch(0.7_0.17_260))]',
       )}
     >
       {appConfig.appTitle}
